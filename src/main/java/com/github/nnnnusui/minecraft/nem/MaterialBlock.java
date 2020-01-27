0b64b31f81b571147398697ac57123abcfb4c81f
nnnnusui <nnnnusui@gmail.com>
2020-01-27 20:35:45 +0900

    ➕ MaterialBlock blockstates 'model' (StringProperty)

--- src/main/java/com/github/nnnnusui/minecraft/nem/MaterialBlock.java
+++ src/main/java/com/github/nnnnusui/minecraft/nem/MaterialBlock.java
@@ -1,14 +1,24 @@
 package com.github.nnnnusui.minecraft.nem;
 
+import com.github.nnnnusui.minecraft.StringProperty;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.state.IProperty;
+import net.minecraft.state.StateContainer;
 
 public class MaterialBlock extends Block {
     static final String registryName = "material";
     static final String propertyName = "model";
+    private static final IProperty<String> modelProperty = StringProperty.create(propertyName, ResourceGenerator.getStateList());
 
     MaterialBlock(){
         super(Block.Properties.create(Material.PLANTS));
         setRegistryName(NotEnoughMaterials.modId, registryName);
+    }
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        super.fillStateContainer(builder);
+        builder.add(MaterialBlock.modelProperty);
     }
 }
