e906b7fd59004cae0f4c3b3e98e4c68394b7dd73
nnnnusui <nnnnusui@gmail.com>
2020-01-27 19:39:32 +0900

    :tada: first commit.

---
+++ src/main/java/com/github/nnnnusui/minecraft/nem/NotEnoughMaterials.java
@@ -0,0 +1,12 @@
+package com.github.nnnnusui.minecraft.nem;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("nem")
+public class NotEnoughMaterials
+{
+//    private static final Logger LOGGER = LogManager.getLogger();
+    static final String modId = "nem";
+
+    public NotEnoughMaterials() { }
+}
