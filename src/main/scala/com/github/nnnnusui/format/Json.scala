706c03c8cd2da7b0292f54857f3c11c6c9c9de5c
nnnnusui <nnnnusui@gmail.com>
2020-03-02 01:43:07 +0900

    :sparkles: Implement Json.parse()

--- src/main/scala/com/github/nnnnusui/format/Json.scala
+++ src/main/scala/com/github/nnnnusui/format/Json.scala
@@ -1,8 +1,21 @@
 package com.github.nnnnusui
 package format
 
+sealed trait Json
 object Json{
-  def parse(text: String) = json.Parser(text)
+  def parse(text: Predef.String) = json.Parser(text)
   def stringify(json: Json) = ???
+
+
+  case class Member(key: String, value: Element) extends Json
+  case class Element(value: Value) extends Json
+
+  sealed trait Value extends Json
+  case class  Object(value: List[Member] ) extends Value
+  case class  Array (value: List[Element]) extends Value
+  case class  String(value: Predef.String) extends Value
+  case class  Number(value: Double       ) extends Value
+  case object True  extends Value
+  case object False extends Value
+  case object Null  extends Value
 }
-trait Json
--- src/main/scala/com/github/nnnnusui/format/json/Parser.scala
+++ src/main/scala/com/github/nnnnusui/format/json/Parser.scala
@@ -3,6 +3,7 @@ package format.json
 
 import com.github.nnnnusui.format.Json
 
+import scala.util.matching.Regex
 import scala.util.parsing.combinator.RegexParsers
 
 private[format] object Parser extends RegexParsers{
@@ -12,6 +13,49 @@ private[format] object Parser extends RegexParsers{
       case Failure( msg    ,_) => Left(s"FAILURE: $msg")
       case Error  ( msg    ,_) => Left(s"ERROR: $msg")
     }
-  val element = ".*".r ^^ (it=> Dummy(s"element: $it"))
+
+  override def skipWhitespace: Boolean = false
+  def ws: Parser[Option[String]] = opt(whiteSpace)
+
+  def element: Parser[Json.Element] = ws ~> value <~ ws ^^ (it=> Json.Element(it))
+
+  def value:   Parser[Json.Value]   = _object | array | string | number | _true | _false | _null
+  def _object: Parser[Json.Object]  = "{"   ~> members    <~ "}"  ^^ (it=> Json.Object(it))
+  def array:   Parser[Json.Array]   = "["   ~> elements   <~ "]"  ^^ (it=> Json.Array(it))
+  def string:  Parser[Json.String]     = "\""  ~> characters <~ "\"" ^^ (it=> Json.String(it))
+  def number:  Parser[Json.Number]  =          double             ^^ (it=> Json.Number(it))
+  val _true:  Parser[Json.True.type]  =       "true"  ^^ (_=> Json.True)
+  val _false: Parser[Json.False.type] =       "false" ^^ (_=> Json.False)
+  val _null:  Parser[Json.Null.type]  =       "null"  ^^ (_=> Json.Null)
+
+  def elements: Parser[List[Json.Element]]  = rep1sep(element, ",") | (ws ^^ (_=> List.empty))
+  def members:  Parser[List[Json.Member]]   = rep1sep(member , ",") | (ws ^^ (_=> List.empty))
+  def member:   Parser[Json.Member]
+    = (ws ~> string <~ ws) ~ (":" ~> element) ^^ {case string ~ element => Json.Member(string, element)}
+
+  def characters: Parser[String] = rep(character) ^^ (_.mkString)
+  def character:  Parser[String] = (".".r - "\"" - "\\") | "\\" ~> escape
+  def escape:     Parser[String] = "\"" | "\\" | "/" | backspace | newPage | carriageReturn | linefeed | tab | unicode
+  def backspace:      Parser[String] = "b" ^^ (_=> "\b")
+  def newPage:        Parser[String] = "f" ^^ (_=> "\f")
+  def carriageReturn: Parser[String] = "n" ^^ (_=> "\n")
+  def linefeed:       Parser[String] = "r" ^^ (_=> "\r")
+  def tab:            Parser[String] = "t" ^^ (_=> "\t")
+  def unicode:        Parser[String] = "u" ~> repN(4, hex) ^^ (it=> BigInt(it.mkString, 16).toChar.toString)
+  def hex:            Parser[String] = digit | "[A-F]".r | "[a-f]".r
+
+  def double:  Parser[Double]
+    = integer ~ opt(fraction) ~ opt(exponent) ^^ { case integer ~ fraction ~ exponent =>
+        s"$integer${fraction.getOrElse("")}${exponent.getOrElse("")}".toDouble
+      }
+  def integer:  Parser[String] = natural | digit | minus
+  def minus:    Parser[String] = "-" ~> (digit | natural) ^^ (it=> s"-$it")
+  def natural:  Parser[String] = oneToNine ~ digits ^^ { case head ~ tail => s"$head$tail" }
+
+  def digits: Parser[String] = rep1(digit) ^^ (_.mkString)
+  def digit:  Parser[String] = "0" | oneToNine
+  def oneToNine: Regex = "[1-9]".r
+  def fraction: Parser[String] = '.' ~> digits ^^ (it=> s".$it")
+  def exponent: Parser[String] = ("E" | "e") ~> opt(sign) ~ digits ^^ {case sign ~ digits => s"e${sign.getOrElse("")}$digits"}
+  def sign:     Parser[String] = "+" | "-"
 }
-case class Dummy(value: String) extends Json
