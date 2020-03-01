46c033042c793faa10441701926c447960517e05
nnnnusui <nnnnusui@gmail.com>
2020-03-02 01:27:01 +0900

    :white_check_mark: Update JsonTest / Add test.json

--- src/test/scala/com/github/nnnnusui/format/JsonTest.scala
+++ src/test/scala/com/github/nnnnusui/format/JsonTest.scala
@@ -1,8 +1,12 @@
 package com.github.nnnnusui
 package format
 
+import java.nio.file.{Files, Paths}
+
 class JsonTest extends org.scalatest.FunSuite {
-  val result = Json.parse(
-    """test""".stripMargin)
+  val testJson = Paths.get("resource", "test.json")
+  import scala.collection.JavaConverters._
+  val json = Files.readAllLines(testJson).asScala.mkString
+  val result = Json.parse(json)
   println(result)
 }
