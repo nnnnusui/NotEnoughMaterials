3a40482a1aafba0a70511ac790126369099db4a8
nnnnusui <nnnnusui@gmail.com>
2020-03-02 23:14:52 +0900

    Change Json.Object key type {Json.String -> Predef.String}

--- src/main/scala/com/github/nnnnusui/format/Json.scala
+++ src/main/scala/com/github/nnnnusui/format/Json.scala
@@ -7,10 +7,10 @@ object Json{
   def stringify(json: Json) = ???
 
   sealed trait Value extends Json
-  case class  Object(value: Map[String, Value]) extends Value
-  case class  Array (value: List[Value]       ) extends Value
-  case class  String(value: Predef.String     ) extends Value
-  case class  Number(value: Double            ) extends Value
+  case class  Object(value: Map[Predef.String, Value]) extends Value
+  case class  Array (value: List[Value]  ) extends Value
+  case class  String(value: Predef.String) extends Value
+  case class  Number(value: Double       ) extends Value
   case object True  extends Value
   case object False extends Value
   case object Null  extends Value
