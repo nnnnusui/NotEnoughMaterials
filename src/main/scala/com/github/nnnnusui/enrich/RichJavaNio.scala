8f03081691f3e13da2369ecdcb8f97d84a914bfa
nnnnusui <nnnnusui@gmail.com>
2020-03-08 06:16:27 +0900

    :sparkles: Path#mkString(String)

--- src/main/scala/com/github/nnnnusui/enrich/RichJavaNio.scala
+++ src/main/scala/com/github/nnnnusui/enrich/RichJavaNio.scala
@@ -2,15 +2,18 @@ package com.github.nnnnusui.enrich
 
 import java.nio.file.{Files, Path}
 
+import scala.collection.JavaConverters._
+
 object RichJavaNio {
   implicit class RichPath(val path: Path){
     def getExtension: String =
       path.getFileName.toString.reverse.takeWhile(_ != '.').reverse
     def getFileNameWithoutExtension: String =
       path.getFileName.toString.takeWhile(_ != '.')
+    def mkString(sep: String): String =
+      path.iterator().asScala.mkString(sep)
   }
   object RichFiles{
-    import scala.collection.JavaConverters._
     def list(path: Path): Seq[Path] =
       if(Files.exists(path)) Files.list(path).iterator().asScala.toSeq
       else                   Seq.empty[Path]
