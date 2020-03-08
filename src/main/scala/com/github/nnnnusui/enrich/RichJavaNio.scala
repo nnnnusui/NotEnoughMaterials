5f343d0492d3ff916b3d70a2d694e33cc6d4c67d
nnnnusui <nnnnusui@gmail.com>
2020-03-08 10:51:08 +0900

    Update: RichJavaNio.scala

--- src/main/scala/com/github/nnnnusui/enrich/RichJavaNio.scala
+++ src/main/scala/com/github/nnnnusui/enrich/RichJavaNio.scala
@@ -1,23 +1,32 @@
 package com.github.nnnnusui.enrich
 
-import java.nio.file.{Files, Path}
+import java.net.URI
+import java.nio.file.{FileSystem, FileSystems, Files, Path, Paths}
 
 import scala.collection.JavaConverters._
 
 object RichJavaNio {
-  implicit class RichPath(val path: Path){
+  implicit class RichPath(val src: Path){
     def extension: String =
-      path.getFileName.toString match {
-        case it if it.contains('.')=> it.split('.').reverse.head.reverse
+      src.fileName.toString match {
+        case it if it.contains('.')=> it.split('.').reverse.head
         case _                     => ""
       }
     def fileNameWithoutExtension: String =
-      path.getFileName.toString match {
+      src.fileName.toString match {
         case it if it.contains('.')=> it.split('.').reverse.tail.reverse.mkString(".")
         case it                    => it
       }
-    def mkString(sep: String): String =
-      path.iterator().asScala.mkString(sep)
+    def toSeq: Seq[Path] = src.iterator().asScala.toSeq
+    def head: Path      = src.toSeq.head
+    def tail: Seq[Path] = src.toSeq.tail
+    def mkString(sep: String): String = src.toSeq.mkString(sep)
+
+    def fileName: Path ={
+      val str = src.getFileName.toString
+      if(str.reverse.head == '/') Paths.get(str.dropRight(1))
+      else                        src.getFileName
+    }
   }
   object RichFiles{
     def list(path: Path): Seq[Path] =
@@ -27,5 +36,9 @@ object RichJavaNio {
       Files.lines(path).iterator().asScala.toSeq
     def walk(path: Path): Seq[Path] =
       Files.walk(path).iterator().asScala.toSeq
+  }
+  object RichFileSystems{
+    def newFileSystem(uri: URI, options: Map[String, Any], loader: ClassLoader): FileSystem =
+      FileSystems.newFileSystem(uri, options.asJava, loader)
   }
 }
