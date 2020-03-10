284d4ddd535ff8b8d91a2711ccef7ac9e2776cf4
nnnnusui <nnnnusui@gmail.com>
2020-03-10 09:25:47 +0900

    :construction: Add ResourcePackConverter

--- src/main/scala/com/github/nnnnusui/minecraft/nem/ResourcePackConverter.scala
+++ src/main/scala/com/github/nnnnusui/minecraft/nem/ResourcePackConverter.scala
@@ -1,14 +1,68 @@
 package com.github.nnnnusui.minecraft.nem
 
-import java.nio.file.Paths
+import java.nio.file.{Path, Paths}
 
+import com.github.nnnnusui.enrich.RichJavaNio._
+import com.github.nnnnusui.format.Json
 import com.github.nnnnusui.minecraft.ResourcePack
+import com.github.nnnnusui.minecraft.resroucepack.{Asset, BlockState, Model, Texture}
 
-class ResourcePackConverter {
-  private val resourceRoot = Paths.get("resource")
-  private val sourceRoot = resourceRoot.resolve("source")
-  private val resultRoot = resourceRoot.resolve("result")
-  private val vanillaRoot = sourceRoot.resolve("vanilla") // TODO: Read from .minecraft directory.
+object ResourcePackConverter {
+  def convertToNem(pack: ResourcePack): ResourcePack =
+    ResourcePack(
+        name   = "NemTest"
+      , assets = {
+          val (states, models, textures) = pack.assets.map{ asset=>
+            val destination = Paths.get(pack.name, asset.name)
+            Paths.get(pack.name, s"${asset.name}")
+            ( toMaterialJsonStates(asset.blockStates, destination)
+            , asset.models   map (model    => convertModelPath(  model  , destination))
+            , asset.textures map (texture  => convertTexturePath(texture, destination)))
+          }.foldLeft((Map.empty[String, Json.Value], Seq.empty[Model], Seq.empty[Texture])){
+            case (before, (states, models, textures)) =>
+              (before._1 ++ states, before._2 ++ models, before._3 ++ textures )
+          }
+          Seq(Asset("nem", Seq(BlockState.fromStates("material", states)), models, textures))
+        }
+    )
 
-  val vanilla: ResourcePack = ResourcePack(vanillaRoot.resolve("1.15.2"))
+  private def toMaterialJsonStates(blockStates: Seq[BlockState], destination: Path): Map[String, Json.Value] = {
+    def searchAndConvert(value: Json.Value): Json.Value = value match {
+      case Json.Object(value) =>
+        value.get("model")
+          .flatMap(_.option[Json.String])
+          .map(it => Json.Object(value.updated("model", Json.String(destination.resolve(it.value).mkString("/")))))
+          .getOrElse(searchAndConvert(Json.Object(value)))
+      case Json.Array(value) => Json.Array(value.map(searchAndConvert))
+      case other => other
+    }
+    blockStates.flatMap(blockState=>
+      blockState.states.map{ case (state, value) =>
+        s"${destination.mkString("/")}:${blockState.name}/$state" -> // TODO: Base32 encode
+        searchAndConvert(value)
+      }
+    ).toMap
+  }
+  private def convertModelPath(model: Model, destination: Path): Model = {
+    def convert(json: Json.Object): Json.Object ={
+      ( for{  optTextures <- json.get("textures")
+              textures    <- optTextures.option[Json.Object]
+        } yield textures.value.map{ case (key, value) => key ->
+          value.option[Json.String]
+            .map(_.value)
+            .map(it=> destination.resolve(it))
+            .map(it=> Json.String(it.mkString("/")))
+            .getOrElse(value)
+        }
+      )
+      .map(textures=> Json.Object(json.value.updated("textures", Json.Object(textures))))
+      .getOrElse(json)
+    }
+    model.copy(
+        path = destination.resolve(model.path).toString
+      , json = convert(model.json)
+    )
+  }
+  private def convertTexturePath(texture: Texture, destination: Path): Texture =
+    texture.copy(path = destination.resolve(texture.path).mkString("/"))
 }
--- src/test/scala/com/github/nnnnusui/minecraft/nem/ResourcePackConverterTest.scala
+++ src/test/scala/com/github/nnnnusui/minecraft/nem/ResourcePackConverterTest.scala
@@ -1,6 +1,29 @@
 package com.github.nnnnusui.minecraft.nem
 
-class ResourcePackConverterTest extends org.scalatest.FunSuite {
-  val converter = new ResourcePackConverter
-  println(converter.vanilla.assets.flatMap(_.blockStates.map(_.name)).mkString("\n"))
+import java.nio.file.Paths
+
+import com.github.nnnnusui.minecraft.ResourcePack
+import org.scalatest.flatspec.AnyFlatSpec
+
+class ResourcePackConverterTest extends AnyFlatSpec {
+  private val resourceRoot = Paths.get("resource")
+  private val sourceRoot = resourceRoot resolve "source"
+  private val targetRoot = resourceRoot resolve "target"
+
+  "test" should "" in {
+    val packName = "1.15.2"
+    val from = sourceRoot resolve "vanilla" resolve packName
+    val pack = ResourcePack(from)
+    val converted = ResourcePackConverter.convertToNem(pack)
+    converted.writeTo(targetRoot)
+  }
+
+//  val dotMinecraft: Path = Paths.get("G:\\game\\Minecraft\\.minecraft")
+//  val versions: Path = dotMinecraft.resolve("versions")
+//  val versionList: Map[String, Path] =
+//    RichFiles.list(versions)
+//    .map(it=> it.resolve(s"${it.fileName}.jar"))
+//    .filter(it=> Files.exists(it))
+//    .map(it=> (s"${it.fileNameWithoutExtension}", it))
+//    .toMap
 }
